b52d58e7e942dc94715ce5b93e8c21c1004244fe
basicsensei <basicsensei@gmail.com>
2012-12-02 22:57:41 +0100

    up to date project.clj

--- project.clj
+++ project.clj
@@ -1,10 +1,20 @@
-(defproject memorystone "1.0.0-SNAPSHOT"
+(defproject clj-memorystone "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies []
-  :dev-dependencies [[org.bukkit/bukkit "1.0.0-R1-SNAPSHOT"]
+  :dependencies [
                      [clj-minecraft "1.0.0-SNAPSHOT"]
-                     [org.clojure/clojure "1.3.0"]
-                     [org.clojure/tools.logging "0.2.3"]]
+                     [org.clojure/clojure "1.4.0"]
+                     [org.clojure/tools.logging "0.2.3"]
+                     [org.bukkit/bukkit "1.4.5-R0.3-SNAPSHOT"]
+                     ]
+  
+  :warn-on-reflection true
+  
   :repl-options [:init nil :caught clj-stacktrace.repl/pst+]
-  :repositories {"spout-repo-snap" "http://repo.getspout.org/content/repositories/snapshots/"
-                 "spout-repo-rel" "http://repo.getspout.org/content/repositories/releases/"})
+  
+  :repositories [
+                 ;"spout-repo-snap" "http://repo.getspout.org/content/repositories/snapshots/"
+                 ;"spout-repo-rel" "http://repo.getspout.org/content/repositories/releases/"
+                 ;"org.bukkit" "Bukkit" "bukkit" {:url "http://repo.bukkit.org/content/groups/public/"}
+                 ["bukkit.snapshots" "http://repo.bukkit.org/content/repositories/snapshots"]
+                 ]
+  )
