c291d3dcdb7ba82d3d8ba096e3d77a51fa64bb6d
basicsensei <basicsensei@gmail.com>
2012-12-03 22:02:52 +0100

    updating scripts

--- lein_uberjar.bat
+++ lein_uberjar.bat
@@ -1,11 +1,12 @@
-@rem this is used to clean then compile then export as .jar  (2 jar files in 'target' folder) 
 @rem needs this lein.bat https://github.com/technomancy/leiningen/blob/master/bin/lein.bat
 	@rem put it in PATH or near this .bat file
 
 @echo off
-
-	
 set JAVA_CMD="c:\program files\java\jdk1.6.0_31\bin\java.exe"
-rem the project should be closed in eclipse+ccw if you're running the cmd on this line: call lein.bat clean
-call lein.bat uberjar
-pause
+call lein uberjar
+
+rem built.jar is a folder link to your running server's plugin folder
+rem you make it by running this cmd: mklink /d built.jar c:\craftbukkit\plugins\
+if EXIST "built.jar" ( move target\memorystone-*standalone*.jar built.jar )
+
+if NOT "%1" == "nopause" @pause
