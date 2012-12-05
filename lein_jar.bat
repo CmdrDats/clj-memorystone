2cbfe060973e64131c384b58d23c17eac899a7da
basicsensei <basicsensei@gmail.com>
2012-12-05 22:33:48 +0100

    lein.bat should be in PATH not in current folder because there is a folder change when calling cljminecraft scripts

--- lein_jar.bat
+++ lein_jar.bat
@@ -1,5 +1,5 @@
 @rem needs this lein.bat https://github.com/technomancy/leiningen/blob/master/bin/lein.bat
-	@rem put it in PATH or near this .bat file
+	@rem put it in PATH
 
 @echo off
 set JAVA_CMD="c:\program files\java\jdk1.6.0_31\bin\java.exe"
