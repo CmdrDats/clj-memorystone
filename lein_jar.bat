e8a2106d38148616f6c4ac618042316ecc87ba81
basicsensei <basicsensei@gmail.com>
2012-12-05 22:30:20 +0100

    use deploy instead of built.jar symlinked folder in scripts

--- lein_jar.bat
+++ lein_jar.bat
@@ -6,10 +6,12 @@ set JAVA_CMD="c:\program files\java\jdk1.6.0_31\bin\java.exe"
 @rem note that this won't be enough(use uberjar instead) if your project deps have anything newer than what cljminecraft project has FIXME:
 call lein jar
 
-rem built.jar is a folder link to your running server's plugin folder
-rem you make it by running this cmd: mklink /d built.jar c:\craftbukkit\plugins\
-if EXIST "built.jar" ( move target\memorystone-*SNAPSHOT.jar built.jar\memorystone.jar )
+set deployDIR=deploy
+rem %deployDIR% is a folder link to your running server's plugin folder
+rem you make it by running this cmd: mklink /d deploy c:\craftbukkit\plugins\
+if EXIST "%deployDIR%" ( move target\memorystone-*SNAPSHOT.jar %deployDIR%\memorystone.jar )
 
+rem this only when other plugins depend on this plugin, put it in local ~/.m2 repository
 rem call lein install
 
 if NOT "%1" == "nopause" @pause
